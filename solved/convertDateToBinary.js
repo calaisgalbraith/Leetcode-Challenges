e3778a46cb05bec2f1099131096a722aa9b37eb6
“calaisgalbraith” <calaisgalbraith@gmail.com>
2024-11-08 09:37:32 -0600

    solved convertDateToBinary

---
+++ solved/convertDateToBinary.js
@@ -0,0 +1,18 @@
+/*
+You are given a string date representing a Gregorian calendar date in the yyyy-mm-dd format.
+
+date can be written in its binary representation obtained by converting year, month, and day to their binary representations without any leading zeroes and writing them down in year-month-day format.
+
+Return the binary representation of date.
+*/
+
+/**
+ * @param {string} date
+ * @return {string}
+ */
+var convertDateToBinary = function(date) {
+    return date
+        .split('-') // split date into array of nums
+        .map(s => (+s).toString(2)) // convert each num to binary
+        .join('-'); // rejoin array into string
+};
